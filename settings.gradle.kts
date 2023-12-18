dd82181c333772f37c9f1ec9746976dfaebbc7c6
Dian Meldayani <dianmeldayani04@gmail.com>
2023-12-18 17:11:19 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Percobaan 8_FireBase_F55122018_Dian Meldayani"
+include(":app")
